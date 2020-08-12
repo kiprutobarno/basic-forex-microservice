2c4b6b51500f6a4236f62b4e9abd22c76180a31e
ywalakamar <barxwells@gmail.com>
2020-08-12 17:11:25 +0300

    chore(ribbon): Load balancing with Ribbon

--- conversion-service/src/main/java/com/ywalakamar/conversion/interfaces/ForexServiceProxy.java
+++ conversion-service/src/main/java/com/ywalakamar/conversion/interfaces/ForexServiceProxy.java
@@ -2,11 +2,13 @@ package com.ywalakamar.conversion.interfaces;
 
 import com.ywalakamar.conversion.model.ConversionDAO;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "forex-service", url = "localhost:8000")
+@RibbonClient(name="forex-service")
 public interface ForexServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ConversionDAO retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
